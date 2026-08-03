60e0341abbebfe9e873517c0e48cffb09e0d2e03
Aamna <aamnafaisal262008@gmail.com>
2026-08-03 14:56:47 +0500

    improvement

--- movie_ticket_calculator.py
+++ movie_ticket_calculator.py
@@ -1,16 +1,16 @@
-class customer:
-    def __init__(self,namep,agep,Is_memberp,Ticket_Amountp):
+class Customer:
+    def __init__(self,namep,agep,is_memberp,ticket_amountp):
         self.name=namep
         self.age=agep
-        self.Is_member= Is_memberp
         self.ticket_type=None
         self.ticket_price=0.0
-        self.Is_member=Is_memberp
-        self.InitialPrice=0.0
-        self.Discount=0.0
-        self.Ticket_Amount=Ticket_Amountp
+        self.is_member=is_memberp
+        self.discount=0.0
+        self.ticket_amount=ticket_amountp
         self.price_of_oneticket=0
-    
+        self.final_price=0
+        self.subtotal=0
+
     def set_ticket_type(self):
                 if self.age <= 4:
                     self.ticket_type="Small Child"
@@ -30,47 +30,58 @@ class customer:
                      else:
                         self.ticket_price=600.0
                      self.price_of_oneticket=self.ticket_price
-                     self.ticket_price=self.ticket_price * self.Ticket_Amount
-                     self.InitialPrice=self.ticket_price
-                     if self.Is_member == True:
-                        self.Discount=self.ticket_price * 0.1
-                        self.ticket_price=self.ticket_price - self.Discount
+                     self.subtotal=self.price_of_oneticket * self.ticket_amount
+                     
+                     if self.is_member == True:
+                        self.discount=self.subtotal * 0.1
+                        self.final_price=self.subtotal - self.discount
 print("Movie Ticket Calculator")
 
 name=input("Please Enter Your Name:")
 Check=False
 while Check == False:
-     age=int(input("Enter Age"))
-     if age <= 0 or age >= 120:
+     try:
+        age=int(input("Enter Age"))
+        if age <= 0 or age >= 120:
           print("please enter a valid age...")
-     else:
-          Check=True
-          
+        else:
+               Check=True 
+     except ValueError:
+          print("Please enter a number")
+
+
+
+            
           
 member=input("Are you a member:Yes or No")
 Check=False
 while Check == False:
     if member.lower() == "yes":
-        Is_member = True
+        is_member = True
         Check = True
     elif member.lower() == "no":
-        Is_member = False
+        is_member = False
         Check=True
     else:
         member=input("please only answer in yes or no.....")
-
-Ticket_Amount=int(input("how many tickets"))
-Customer=customer(name,age,Is_member,Ticket_Amount)
-Customer.set_ticket_type()
-Customer.set_ticket_price()
-print("Reciept".center(20,"_"))
-print(f"customer:{Customer.name}")
-print(f"Category:{Customer.ticket_type}")
-print(f"Price per ticket:{Customer.price_of_oneticket}")
-print(f"Tickets:{Customer.Ticket_Amount}")
-print(f"Subtotal:{Customer.InitialPrice}")
-print(f"Discount:{Customer.Discount}")
-print(f"Amount Payable:{Customer.ticket_price}")
+Check=False
+while Check == False:
+     ticket_amount=int(input("how many tickets"))
+     if ticket_amount <= 0:
+          print("Invalid Ticket Number,Please Try Again (value should be greater than 0)")
+     else:
+          Check=True
+customer_data=Customer(name,age,is_member,ticket_amount)
+customer_data.set_ticket_type()
+customer_data.set_ticket_price()
+print("Receipt".center(20,"_"))
+print(f"customer:{customer_data.name}")
+print(f"Category:{customer_data.ticket_type}")
+print(f"Price per ticket:{customer_data.price_of_oneticket:.2f}")
+print(f"Tickets:{customer_data.ticket_amount:.2f}")
+print(f"Subtotal:{customer_data.subtotal:.2f}")
+print(f"Discount:{customer_data.discount:.2f}")
+print(f"Amount Payable:{customer_data.final_price:.2f}")
 
 
 
