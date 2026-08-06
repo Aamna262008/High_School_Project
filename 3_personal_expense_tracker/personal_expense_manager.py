c8a15f07e84165b547eef7948f4412b683a2e669
Aamna <aamnafaisal262008@gmail.com>
2026-08-06 11:00:21 +0500

    Spending Summary Added

--- 3_personal_expense_tracker/personal_expense_manager.py
+++ 3_personal_expense_tracker/personal_expense_manager.py
@@ -189,6 +189,94 @@ def search_expenses():
             print(f"|{headers[0]:<10}|{headers[1]:<10}|{headers[2]:<20}|{headers[3]:<20}|{headers[4]:<10}|{headers[5]:<10}")
             for row in list_search:
                 print(f"|{row[0]:<10}|{row[1]:<10}|{row[2]:<20}|{row[3]:<20}|{row[4]:<10}|{row[5]:<10}")
+def spending_summary():
+    grand_total=0
+    num_of_expense=0
+    avg_expense=0
+    total_food=0
+    total_transport=0
+    total_education=0
+    total_entertainment=0
+    total_shopping=0
+    total_bills=0
+    total_health=0
+    total_other=0
+    expense_list=[]
+    category_expense_list=[]
+    highest_spending_category=[]
+    max_expense=[]
+    largest_expense=[]
+    with open("expense_list.csv",'r') as file:
+        reader=csv.reader(file)
+        rows=list(reader)
+        for row in rows[1:]:
+            grand_total=grand_total + float(row[4])
+            expense_list.append(float(row[4]))
+
+       
+            num_of_expense=num_of_expense+1
+            if row[2] == "Food"  :
+                total_food = total_food + float(row[4])
+            elif row[2] == "Transport" :
+               total_transport = total_transport + float(row[4])
+            elif row[2] == "Education":
+                total_education=total_education+float(row[4])
+            elif row[2] == "Entertainment" :
+                total_entertainment=total_entertainment+float(row[4])
+            elif row[2] == "Shopping":
+                total_shopping=total_shopping+float(row[4])
+            elif row[2] =="Bills":
+                total_bills=total_bills+float(row[4])
+            elif row[2] == "Health":
+                total_health=total_health+float(row[4])
+            elif row[2] == "Other":
+                total_other=total_other+float(row[4])
+        category_expense_list=[("Food",total_food),("Transport",total_transport),("Education",total_education),("Entertainment",total_entertainment),("Shopping",total_shopping),("Bills",total_bills),("Health",total_health),("Other",total_other)]
+        highest=max(category[1] for category in category_expense_list)
+        for category,amount in category_expense_list:
+           if amount == highest:
+             
+             highest_spending_category.append((category,amount))
+        try:
+           max_expense=max(expense_list)
+        except ValueError:
+            print("Empty File")
+        for row in rows[1:]:
+            if float(row[4]) == max_expense:
+                largest_expense.append(row)
+        try:
+            avg_expense=grand_total/num_of_expense
+        except ZeroDivisionError:
+            print("No expense") 
+        print(f"Grand_Total:{grand_total}")
+        print(f"Total_Expenses:{num_of_expense}")
+        print(f"Total Expense Of Food:{total_food}")
+        print(f"Total Expense Of Transport:{total_transport}")
+        print(f"Total Expense Of Education:{total_education}")
+        print(f"Total Expense Of Entertainment:{total_entertainment}")
+        print(f"Total Expense Of Shopping:{total_shopping}")
+        print(f"Total Expense Of Bills:{total_bills}")
+        print(f"Total Expense Of Health:{total_health}")
+        print(f"Other Expenses:{total_other}") 
+        for list_ in highest_spending_category:
+            print(f"Category with most amount of money spent:{list_}")
+        for list_ in largest_expense:
+            print(f"Largest Expense\n{list_}")  
+               
+
+            
+
+
+  
+
+            
+
+
+
+   
+                
+
+                
 
 
 
